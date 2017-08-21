88eaa45035c49211382d3a22e71437a8c434d912
heyumNakt <nakt@heyumsys.com>
2017-08-21 17:53:16 +0900

    test
    commit test

---
+++ test/src/main/java/test/example.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.heyum.mecury.history.HistoryPlugin;
+
+public class example {
+    @Autowired
+    HistoryPlugin dao;
+
+	var testa = "testaaaaaaaa";
+}
